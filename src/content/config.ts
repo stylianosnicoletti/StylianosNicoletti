5472b07a79ac9e9facc3ef0597222740f50d4093
stylianosnicoletti <35661905+stylianosnicoletti@users.noreply.github.com>
2023-07-21 12:18:48 +0300

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,6 @@ const projects = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string().optional(),
 		heroImage: z.string().optional(),
 	}),
 });
